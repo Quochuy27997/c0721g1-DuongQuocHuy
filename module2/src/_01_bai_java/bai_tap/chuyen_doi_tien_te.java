b96d762023b4f145b962410adb6ec6624e661269
Huy Duong <quochuyduongdn97@gmail.com>
2021-08-16 11:41:31 +0700

    nop bai ngay16/8

---
+++ module2/src/_01_bai_java/bai_tap/chuyen_doi_tien_te.java
@@ -0,0 +1,12 @@
+package _01_bai_java.bai_tap;
+import java.util.Scanner;
+public class chuyen_doi_tien_te {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        final int menhgia= 23000;
+        System.out.print("hay nhập số tiền cần đổi");
+        int usd =scanner.nextInt();
+        int vnd =usd*menhgia;
+        System.out.print("số tiền Vnd là:"+vnd+"Vnd");
+    }
+}
